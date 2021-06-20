12b7e36349928c89d0ed37a6c0dcde7cfa789948
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-06-20 10:35:23 +0300

    Get one person

--- src/index.js
+++ src/index.js
@@ -16,6 +16,11 @@ app.get('/api/people', function (_req, res) {
     res.json({ people: getPeople() });
 });
 
+app.get('/api/people/:id', function (req, res) {
+	const person = getPeople().find(p => p.id === parseInt(req.params.id));
+    res.json({ person });
+});
+
 app.post('/api/people', function (req, res) {
     const people = getPeople();
     let maxId = people.reduce((max, p) => p.id > max ? p.id : max, people[0].id);
