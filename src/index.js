e4b503a092cecf7704ea01aaa3f91c74ee36cef1
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-07-18 13:32:19 +0300

    Events

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import express from "express";
-import { json, urlencoded } from "body-parser";
-import fs from 'fs';
+import bodyParser from "body-parser";
 
 import { CORS, loadDataFromFile, saveDataToFile } from "./commons";
+
+const { json, urlencoded } = bodyParser;
 
 const app = express();
 app.use(urlencoded({ extended: false }));
 app.use(json());
 app.use(CORS);
+
+const statuses = [
+	'Submited',
+	'Designated to',
+	'Waiting for supply',
+	'Works in progress',
+	'Works completed',
+	'Invoiced',
+	'Closed'
+];
 
 const loadUsers = () => loadDataFromFile('data/users.json');
 const saveUsers = users => saveDataToFile('data/users.json', users);
@@ -15,13 +26,70 @@ const saveUsers = users => saveDataToFile('data/users.json', users);
 const loadDevices = () => loadDataFromFile('data/devices.json');
 const saveDevices = users => saveDataToFile('data/devices.json', users);
 
+const loadEvents = () => loadDataFromFile('data/events.json');
+const saveEvents = events => saveDataToFile('data/events.json', events);
+
+const generateRandomEvents = () => {
+	const rand = Math.floor(Math.random() * 10);
+	if (rand > 7) {
+		const devices = loadDevices();
+		if (devices.length <= 0) {
+			console.error('No devices found');
+			return;
+		}
+		const events = loadEvents();
+		let maxId = events.reduce((max, e) => e.id > max ? e.id : max, events?.[0]?.id || 0);
+		const event = {
+			id: maxId + 1,
+			title: 'Event ' + rand,
+			deviceId: devices[Math.floor(Math.random() * devices.length)].id,
+			status: {
+				title: statuses[Math.floor(Math.random() * statuses.length)],
+				importance: Math.floor(Math.random() * 9) + 1
+			}
+		};
+		events.push(event);
+		saveEvents(events);
+	}
+};
+
+app.get('/api/events', function (_req, res) {
+	generateRandomEvents();
+	res.json({ events: loadEvents() });
+});
+
+app.post('/api/events', function ({ body }, res) {
+	const events = loadEvents();
+
+	let event = null;
+	if (body.id) {
+		const index = events.findIndex(e => e.id === parseInt(body.id));
+		event = events[index];
+		for (const key in body) {
+			event[key] = body[key];
+		}
+	} else {
+		let maxId = events.reduce((max, e) => e.id > max ? e.id : max, events?.[0]?.id || 0);
+		event = { id: maxId + 1, ...body };
+		events.push(event);
+	}
+	saveEvents(events);
+
+	res.json(event);
+});
+
+app.delete('/api/events/:id', function (req, res) {
+	saveEvents(loadEvents().filter(({ id }) => id !== parseInt(req.params.id)));
+	res.json({ success: 'deteled' });
+});
+
 app.get('/api/users', function (_req, res) {
-    res.json({ users: loadUsers() });
+	res.json({ users: loadUsers() });
 });
 
 app.get('/api/users/:id', function (req, res) {
 	const user = loadUsers().find(u => u.id === parseInt(req.params.id));
-    res.json({ user });
+	res.json({ user });
 });
 
 app.post('/api/users', function ({ body }, res) {
@@ -45,17 +113,17 @@ app.post('/api/users', function ({ body }, res) {
 });
 
 app.delete('/api/users/:id', function (req, res) {
-    saveUsers(loadUsers().filter(({ id }) => id !== parseInt(req.params.id)));
-    res.json({ success: 'deteled' });
+	saveUsers(loadUsers().filter(({ id }) => id !== parseInt(req.params.id)));
+	res.json({ success: 'deteled' });
 });
 
 app.get('/api/devices', function (_req, res) {
-    res.json({ devices: loadDevices() });
+	res.json({ devices: loadDevices() });
 });
 
 app.get('/api/devices/:id', function (req, res) {
 	const device = loadDevices().find(d => d.id === parseInt(req.params.id));
-    res.json({ device });
+	res.json({ device });
 });
 
 app.post('/api/devices', function ({ body }, res) {
@@ -79,12 +147,12 @@ app.post('/api/devices', function ({ body }, res) {
 });
 
 app.delete('/api/devices/:id', function (req, res) {
-    saveDevices(loadDevices().filter(({ id }) => id !== parseInt(req.params.id)));
-    res.json({ success: 'deteled' });
+	saveDevices(loadDevices().filter(({ id }) => id !== parseInt(req.params.id)));
+	res.json({ success: 'deteled' });
 });
 
 app.listen(3000, () => {
-    console.log("App started");
+	console.log("App started");
 });
 
 export default app;
