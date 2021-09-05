06a9f7b7b0592962aec8e97b4a7d9b0ec349acc2
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-05 12:19:36 +0300

    Commons

--- src/commons.js
+++ src/commons.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 export const CORS = (
   _req,
   res,
@@ -7,4 +9,55 @@ export const CORS = (
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "*");
   next();
+};
+
+export const crud = (app, resourceName) => {
+  getAll(app, resourceName);
+  getOne(app, resourceName);
+  remove(app, resourceName);
+  post(app, resourceName);
+};
+
+const loadDataFromFile = file => fs.existsSync(file) ? JSON.parse(fs.readFileSync(file)) : [];
+const saveDataToFile = (file, data) => fs.writeFileSync(file, JSON.stringify(data));
+
+const getAll = (app, resourceName) => {
+  app.get(`/api/${resourceName}`, function (_req, res) {
+    res.json({ data: loadDataFromFile(`data/${resourceName}.json`) });
+  });
+};
+
+const getOne = (app, resourceName) => {
+  app.get(`/api/${resourceName}/:id`, function (req, res) {
+    res.json({ data: loadDataFromFile(`data/${resourceName}.json`).find(i => i.id === parseInt(req.params.id)) });
+  });
+};
+
+const post = (app, resourceName) => {
+  app.post(`/api/${resourceName}`, function ({ body }, res) {
+    const data = loadDataFromFile(`data/${resourceName}.json`);
+  
+    let record = null;
+    if (body.id) {
+      const index = data.findIndex(i => i.id === parseInt(body.id));
+      record = data[index];
+      for (const key in body) {
+        record[key] = body[key];
+      }
+    } else {
+      let maxId = data.reduce((max, i) => i.id > max ? i.id : max, data?.[0]?.id || 0);
+      record = { ...body, id: maxId + 1 };
+      data.push(record);
+    }
+    saveDataToFile(`data/${resourceName}.json`, data);
+  
+    res.json({ data: record });
+  });
+};
+
+const remove = (app, resourceName) => {
+  app.delete(`/api/${resourceName}/:id`, function (req, res) {
+    saveDataToFile(`data/${resourceName}.json`, loadDataFromFile(`data/${resourceName}.json`).filter(({ id }) => id !== parseInt(req.params.id)));
+    res.json({ success: 'deteled' });
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,55 +1,16 @@
 import express from "express";
-import { json, urlencoded } from "body-parser";
-import fs from 'fs';
+import bodyParser from "body-parser";
 
-import { CORS } from "./commons";
+import { CORS, crud } from "./commons";
+
+const { json, urlencoded } = bodyParser;
 
 const app = express();
 app.use(urlencoded({ extended: false }));
 app.use(json());
 app.use(CORS);
 
-const getPeople = () => JSON.parse(fs.readFileSync('data/people.json'));
-const savePeople = people => fs.writeFileSync('data/people.json', JSON.stringify(people));
-
-app.get('/api/people', function (_req, res) {
-    res.json({ people: getPeople() });
-});
-
-app.get('/api/people/:id', function (req, res) {
-	const person = getPeople().find(p => p.id === parseInt(req.params.id));
-    res.json({ person });
-});
-
-app.post('/api/people', function (req, res) {
-	const people = getPeople();
-	let person = null;
-	if (req.body.id) {
-		const index = people.findIndex(p => p.id === parseInt(req.body.id));
-		person = people[index];
-		for (const key in req.body) {
-			person[key] = req.body[key];
-		}
-	} else {
-		let maxId = people.reduce((max, p) => p.id > max ? p.id : max, people[0].id);
-		person = { id: maxId + 1, ...req.body };
-		people.push(person);
-	}
-	let maxCarId = -1;
-	people.forEach(p => {
-		if (p && p.cars) {
-			p.cars.forEach(c => maxCarId = (c && c.id && c.id > maxCarId) ? c.id : maxCarId)
-		}
-	});
-	person.cars.forEach(c => c.id = c.id ? c.id : ++maxCarId);
-	savePeople(people);
-	res.json(person);
-});
-
-app.delete('/api/people/:id', function (req, res) {
-    savePeople(getPeople().filter(({ id }) => id !== parseInt(req.params.id)));
-    res.json({ success: 'deteled' });
-});
+crud(app, 'people');
 
 app.listen(3000, () => {
     console.log("App started");
